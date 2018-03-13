20afc6ebf4d1ec70533630aa84d168d4eb90da35
nblxanueb <a.e.pykhantsev@gmail.com>
2018-03-13 22:21:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function happyHolidayTo(holiday,name) {
 
 }
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 
 }
